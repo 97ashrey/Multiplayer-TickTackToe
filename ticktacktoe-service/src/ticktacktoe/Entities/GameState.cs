0f28376ee3d6151cfd8fd1228e286f549fcd19ed
Aleksandar <32496425+97ashrey@users.noreply.github.com>
2020-06-28 21:27:23 +0200

    Implemented model for game state (#1)

---
+++ ticktacktoe-service/src/ticktacktoe/Entities/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ticktacktoe.Entities
+{
+    public class GameState
+    {
+        public Guid ID { get; set; }
+
+        public List<Player> Players { get; set; }
+
+        public Player CurrentPlayer { get; set; }
+
+        public List<string> Board { get; set; }
+    }
+}
---
+++ ticktacktoe-service/src/ticktacktoe/Entities/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ticktacktoe.Entities
+{
+    public class Player
+    {
+        public string ConnectionID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Score { get; set; } = 0;
+    }
+}
