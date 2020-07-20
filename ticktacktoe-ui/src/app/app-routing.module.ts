e492c90cd86ccd7d2f5ceb6f4e7c0511200d1b68
unknown <97ashrey@gmail.com>
2020-07-20 19:33:57 +0200

    Set up routing

---
+++ ticktacktoe-ui/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { GamePageComponent } from './pages/game-page/game-page.component';
+
+// sets up routes constant where you define your routes
+const routes: Routes = [
+    { path: 'home', component: HomePageComponent },
+    { path: 'game/:gameId', component: GamePageComponent }
+]; 
+
+// configures NgModule imports and exports
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
