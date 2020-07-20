ec04b82dc67f8fa7bba9d09cccd7a48ccd91770a
Aleksandar <32496425+97ashrey@users.noreply.github.com>
2020-07-20 19:34:22 +0200

    Set up routing (#5)

--- ticktacktoe-ui/src/app/app.module.ts
+++ ticktacktoe-ui/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { GamePageComponent } from './pages/game-page/game-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    GamePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ticktacktoe-ui/src/app/pages/game-page/game-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-game-page',
+  templateUrl: './game-page.component.html',
+  styleUrls: ['./game-page.component.css']
+})
+export class GamePageComponent implements OnInit {
+
+  public gameId: string;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.gameId = this.route.snapshot.paramMap.get('gameId');
+  }
+
+}
